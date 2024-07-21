7425b884e5ce38cd2586251316464b74e8e5b9bc
Rajanbabu <rajanbabu4111997@gmail.com>
2024-07-21 15:16:54 +0530

    Add project files.

---
+++ ReactwithASP/ClientApp/src/components/GridView.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import ProductView from '../components/ProductView';
+const GridView = () => {
+    const [Products, setProducts] = useState([]);
+    const [viewProduct, setViewProduct] = useState(false);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((json) => {console.log(json); setProducts(json); setViewProduct(true); console.log(Products)});
+  }, []);
+
+  return (
+    <div>
+    { viewProduct ?
+       <div>
+        {
+        Products.map((product) => (
+            <ProductView Product = {product}></ProductView>
+           ))
+        }
+    </div>:
+    <div>
+      Loading.........
+      <div class="loader"></div>
+    </div>
+}
+    </div>
+  )
+}
+
+export default GridView
---
+++ ReactwithASP/ClientApp/src/components/ShopData.js
@@ -0,0 +1,22 @@
+﻿import React, { useState } from 'react';
+import '../App.css';
+import GridView from './GridView';
+
+function ShopData() {
+    const [Productvisible, setProductvisible] = useState(false);
+    const [Buttonvisible, setButtonvisible] = useState(false);
+    function onloadDate() {
+        setProductvisible(true);
+        setButtonvisible(true);
+    }
+    return (
+        <div className='App'>
+            <div hidden={Buttonvisible}>Welcome to my page</div>
+            <button className='button' onClick={onloadDate} hidden={Buttonvisible}>Click here to load Data</button>
+            {Productvisible ? <GridView /> : <div></div>}
+        </div>
+
+    );
+}
+
+export default ShopData;
